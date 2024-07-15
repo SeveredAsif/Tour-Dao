26390fc9a694888b9c255a8358cdfd5b786a1852
Othiya <othiya2003@gmail.com>
2024-07-15 22:05:38 +0600

    hotelbooking

--- client/src/Pages/HotelDetails.jsx
+++ client/src/Pages/HotelDetails.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Navbar from '../Components/Navbar';
 import axios from 'axios';
-import { Link,useNavigate, useLocation } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 
 const HotelDetails = () => {
   const [hotels, setHotels] = useState([]);
